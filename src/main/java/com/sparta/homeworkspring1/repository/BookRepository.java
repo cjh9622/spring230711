92487278f4bb8959f03d7a862451b261fcd8a3d3
cjh9622 <cjh9622@gmail.com>
2023-07-11 15:17:58 +0900

    Revert "도서관리 프로그램"
    This reverts commit c83c0458a136bbcfe569ff82f8f1292934113c6a.

--- src/main/java/com/sparta/homeworkspring1/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.homeworkspring1.repository;
-
-import com.sparta.homeworkspring1.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
